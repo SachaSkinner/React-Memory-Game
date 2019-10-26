803379406d40836ff9fa298f7901f57c5cc00e5e
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-10-25 23:48:04 -0500

    added best result and current result states with alert for now

--- src/App.js
+++ src/App.js
@@ -9,25 +9,52 @@ class App extends Component {
   // Setting this.state.characters to the characters json array
   state = {
     characters,
-    clicked: []
+    clicked: [],
+    bestResult: 0,
+    currentResult: 0
   };
+  checkIfWins =()=> {
+    if(this.state.currentResult === 9){
+      alert(" You win!!!")
+      this.setState({ bestResult: this.state.currentResult })
+
+    }
+  }
 
   checkIfRepeat = (id) => {
-    if (this.state.clicked.includes(id)) {      
+    if (this.state.clicked.includes(id)) {
       return true;
     }
     return false;
   }
 
   clickCard = (id) => {
-    if(!this.checkIfRepeat(id)){
+    if (!this.checkIfRepeat(id)) {
       this.state.clicked.push(id);
       this.setState({ clicked: this.state.clicked });
       this.shuffleAll();
+      this.setState({ currentResult: this.state.clicked.length })
+      this.checkIfWins()
+      
+
+
+
+
     } else {
       alert("Oops..Try again!");
       this.setState({ clicked: [] });
-      this.setState({characters: characters.sort((a,b) => { return a.id - b.id; })});
+      this.setState({ characters: characters.sort((a, b) => { return a.id - b.id; }) });
+      this.setState({ bestResult: this.state.currentResult })
+      // if (this.state.currentResult === 9) {
+      //   alert("Woohoo, you won!!")
+      // }
+      // else (this.state.currentResult > this.state.bestResult){
+      //   this.setState({ bestResult: this.state.currentResult })
+
+      // }
+
+
+
     }
 
   }
@@ -45,13 +72,19 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Title>React Memory Game!</Title>
+        <Title>React Memory Game!
+         <br></br> Your best result is {this.state.bestResult} from 9!
+         <br></br>
+          Your current result is {this.state.currentResult} from 9.
+        </Title>
         {this.state.characters.map(character => (
           <CharacterCard
             clickCard={this.clickCard}
             image={character.image}
             key={character.id}
             id={character.id}
+
+
           />
         ))}
       </Wrapper>
--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -5,6 +5,7 @@ function CharacterCard(props) {
   return (
     <div className="card">
       <div className="img-container">
+      
         <img src={props.image} alt="character" onClick={() => {props.clickCard(props.id)}} />
       </div>
       
