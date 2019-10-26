2b822f08cbe3b107cc7167e6d31b957806e731ca
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-10-25 20:50:16 -0500

    added a shuffle function with a new setState, added an alt prop for pictures, created a new state for clicked ids-s as an array, added a key prop for a card object

--- src/App.js
+++ src/App.js
@@ -4,26 +4,42 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import characters from "./characters.json";
 
+
 class App extends Component {
   // Setting this.state.characters to the characters json array
   state = {
-    characters
+    characters,
+    clicked: []
   };
-  
 
+  clickCard = (id) => {
+    console.log(id)
+    this.state.clicked.push(id);
+    this.setState({clicked: this.state.clicked});
+
+    this.shuffleAll();
+  }
+
+  shuffleAll = () => {
+    let shuffledCharacters = this.state.characters.sort(function func(a, b) {  
+      return 0.5 - Math.random();
+    });
+    
+    this.setState({characters: shuffledCharacters});
+
+  }
 
   // Map over this.state.characters and render a CharacterCard component for each character object
   render() {
     return (
       <Wrapper>
-        <Title>React Memory Game!</Title>
+        <Title>React Memory Game!</Title>   
         {this.state.characters.map(character => (
           <CharacterCard
-        
-            id={character.id}
-            image={character.image}
-
-            
+            clickCard={this.clickCard}        
+            image={character.image} 
+            key={character.id} 
+            id={character.id}           
           />
         ))}
       </Wrapper>
--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -5,11 +5,13 @@ function CharacterCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img src={props.image} />
+        <img src={props.image} alt="character" onClick={() => {props.clickCard(props.id)}} />
       </div>
       
     </div>
   );
 }
 
+
 export default CharacterCard;
+
